46fd716ee6dfa9ba94daf0e04ad6c56f1883fded
Yash Sahu <sahu.yash0205@gmail.com>
2024-11-21 19:49:45 +0530

    Updating structure

--- main.py
+++ main.py
@@ -17,6 +17,22 @@ def login():
 def add_user():
     return render_template('adminLogin.html')
 
+@app.route('/admin_dashboard')
+def go_to_admin_dashboard():
+    return render_template('adminDashboard.html')
+
+@app.route('/admin_teachers')
+def go_to_admin_teachers():
+    return render_template('adminTeachers.html')
+
+@app.route('/admin_students')
+def go_to_admin_students():
+    return render_template('adminStudents.html')
+
+# @app.route('/admin_dashboard')
+# def go_to_admin_dashboard():
+#     return render_template('adminDashboard.html')
+
 @app.route('/home')
 def home():
     if 'user_id' in session:
@@ -29,7 +45,7 @@ def admin_login_validation():
     code=request.form.get('code')
     
     if code=="9460":
-        return render_template("adminHome.html")
+        return render_template("adminDashboard.html")
     else:
         return "Wrong Code! Enter the valid code..." 
 
@@ -102,15 +118,11 @@ def register():
         # cursor.execute("""SELECT * FROM `login` WHERE `userid` LIKE '{}'""".format(userid))
         # myuser=cursor.fetchall()
         # session['user_id']=myuser[0][0]
-        # return redirect('/home')
-        
-        
+        # return redirect('/home')   
     else:
         return "Wrong Code!.. Please Enter the valid code...."
     
     
-    
-
 @app.route('/logout',methods=['POST'])
 def logout():
     # session.pop('code')
@@ -147,6 +159,70 @@ def search_teacher():
             # return render_template('teacherDetail.html',Result=result)
             return render_template('adminHome.html',showTeacherDetail_visibility="visible",showTeacherDetail_display="block",Result=result,anchor="teacher")
 
+
+@app.route('/show_update_teacher_details_form')
+def show_update_teacher_details_form():
+    return render_template('updateTeacherDetails.html')
+
+@app.route('/update_teacher_details',methods=['POST'])
+def update_teacher_details():
+    userid=request.form.get('userid')  
+    userid=userid.replace(' ','')
+    password=request.form.get('password')
+    fname=request.form.get('fname')
+    lname=request.form.get('lname')
+    email=request.form.get('email')
+    phone=request.form.get('phone')
+    # phone=phone.replace('-','')
+    gender=request.form.get('gender')
+    dname=request.form.get('dname')
+    address=request.form.get('address')
+    
+     
+    cursor.execute("""SELECT * FROM `login` WHERE `USERID` LIKE '{}'""".format(userid))
+    user_login_detail=cursor.fetchall()
+    
+    if(len(user_login_detail)>0):
+        
+        category=user_login_detail[0][2]
+        if(category=="F"):
+            cursor.execute("""SELECT * FROM `facultyinfo` WHERE `userid` LIKE '{}'""".format(userid))
+            user_details=cursor.fetchall()
+            if(len(user_details)>0):
+                ## len(user_details)>0 so Here details are available it means we need to update it
+                try:
+                    # Update details
+                    cursor.execute("""UPDATE `facultyinfo` SET `fname` = %s, `lname` = %s, `email` = %s, `phone` = %s, `dname` = %s, `gender` = %s, `address` = %s WHERE userid = %s""",(fname,lname,email,phone,dname,gender,address,userid))
+                    # Update password
+                    cursor.execute("""UPDATE `login` SET password = %s WHERE userid = %s""",(password,userid))
+                    conn.commit()
+                    return "Details updated successfully."
+                
+                except Exception as e:
+                    conn.rollback()
+                    return f"An error occurred: {str(e)}"
+            
+            else:
+                ## else we will add details
+                try:
+                    cursor.execute("""INSERT INTO `facultyinfo`(`userid`,`fname`,`lname`,`email`,`phone`,`dname`,`gender`,`address`) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)""",(userid,fname,lname,email,phone,dname,gender,address))
+                    conn.commit()
+                    return "Details added successfully."
+                        
+                except Exception as e:
+                    conn.rollback()
+                    return f"An error occurred: {str(e)}"
+                
+
+
+        else:
+            return "This is userid of a student"
+                                
+    else:
+        return "No user for this userid....<a href=''>Try Again!</a>"
+            
+    
+    
 
 if __name__=="__main__":
     app.run(debug=True)
